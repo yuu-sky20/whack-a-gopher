98c279c85d99080be83086597794bb043a5fc274
yuu_sky <yuusky20th@gmail.com>
2022-11-06 11:25:44 +0900

    rename file

--- app/models/character.tsx
+++ app/models/character.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styled from "styled-components";
-import ICharacter from "./interfaces/character";
+import ICharacter from "./interfaces/ICharacter";
 
 export default function Character(props: ICharacter) {
 
