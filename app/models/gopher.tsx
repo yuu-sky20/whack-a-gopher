a84266a80886e87cb66b03e5213c9c0bf1aacd59
yuu_sky <yuusky20th@gmail.com>
2022-11-06 12:09:53 +0900

    fix image src

--- app/models/gopher.tsx
+++ app/models/gopher.tsx
@@ -2,6 +2,6 @@ import Character from "./character";
 
 export default function Gopher() {
     return (
-        <Character image_src="/public/gopher.png" character_name="gopher" />
+        <Character image_src="/gopher.png" character_name="gopher" />
     )
 }
--- app/models/lisp-alien.tsx
+++ app/models/lisp-alien.tsx
@@ -3,6 +3,6 @@ import Character from "./character";
 
 export default function Gopher() {
     return (
-        <Character image_src="/public/lisp-alien.png" character_name="lisp-alien" />
+        <Character image_src="/lisp-alien.png" character_name="lisp-alien" />
     )
 }
