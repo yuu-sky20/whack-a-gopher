81dcf0952fbe0672effb12c0c0270fab02fe18d6
yuu_sky <yuusky20th@gmail.com>
2022-11-07 15:48:12 +0900

    tentative

--- app/models/platform.tsx
+++ app/models/platform.tsx
@@ -1,14 +1,19 @@
 import Hole from "./hole"
 import Gopher from "./gopher"
 import LispAlien from "./lisp-alien"
+import styled from "styled-components"
 
 export default function Platform() {
     const plant_num = 9;
 
+    const Container = styled.div`
+        display: flex;
+        align-items: center;
+    `
 
     return (
-        <div>
-
-        </div>
+        <Container>
+            <Hole></Hole>
+        </Container>
     )
 }
