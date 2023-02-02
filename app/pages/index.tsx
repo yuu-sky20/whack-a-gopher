fb872e44c5acff10f57cb386b407e816bcf30499
yuu_sky <yuusky20th@gmail.com>
2023-02-03 00:05:13 +0900

    add link

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,17 +1,30 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styled from 'styled-components'
-import Platform from './templates/platform'
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Whack-a-Gopher</title>
-        <meta name="description" content="whack-a-gopher app here." />
+        <meta name="description" content="whack-a-gopher title scene" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
+        <div>
+          <h1>Whack a Gopher</h1>
+          <h4>and Lisp Alien</h4>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link href="/game/main">Game Start!</Link>
+            </li>
+            <li>
+              <Link href="/game/result">Result Menu</Link>
+            </li>
+          </ul>
+        </div>
       </main>
     </div>
   )
